4cb85eed5e80a76c3877f2e7af38edc9ba275996
Pedro Poças <p.pedro@ipvc.pt>
2025-04-07 22:53:37 +0100

    change style CreateAtivoo.cshtml

--- AtivosFinanceiros/Program.cs
+++ AtivosFinanceiros/Program.cs
@@ -55,6 +55,6 @@ app.UseSession(); // Initialize session middleware here
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Auth}/{action=Login}/{id?}");
 
 app.Run();
